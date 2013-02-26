1e150499b77df445798f5830bd396cee6dba51d0
Bryan Duggan <skooter500@gmail.com>
2013-02-26 17:01:29 +0000

    Bla

---
+++ Steering/Steering/Steering/Cell.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Steering
+{
+    public class Cell
+    {
+        public BoundingBox Box = new BoundingBox();
+        int number;
+        List<Cell> adjacent = new List<Cell>();
+
+        public List<Cell> Adjacent
+        {
+            get { return adjacent; }
+            set { adjacent = value; }
+        }
+
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public bool Contains(Vector3 pos)
+        {
+            ContainmentType type = Box.Contains(pos);
+            return ((type == ContainmentType.Contains) || (type == ContainmentType.Intersects));
+        }
+    }
+}
---
+++ Steering/Steering/Steering/Space.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Steering
+{
+    public class Space
+    {
+        List<Cell> cells = new List<Cell>();
+
+        public List<Cell> Cells
+        {
+            get { return cells; }
+            set { cells = value; }
+        }
+
+        public Space()
+        {
+            // generate the list of cells
+            float cellWidth = Params.GetFloat("cell_width");
+            float range = Params.GetFloat("world_range");
+            float worldWidth = range * 2;
+
+            int num = 0;
+            for (float y = -range; y < range; y+= cellWidth)
+            {
+                for (float z = -range; z < range; z += cellWidth)
+                {
+                    for (float x = -range; x < range; x += cellWidth)
+                    {
+                        Cell cell = new Cell();
+                        cell.Box.Min = new Vector3(x, y, z);
+                        cell.Box.Max = new Vector3(x + cellWidth, y + cellWidth, z + cellWidth);
+                        cell.Number = num++;
+                        cells.Add(cell);
+                    }
+                }
+            }
+            // Set up adjacent cells
+            foreach (Cell cell in cells)
+            {
+                BoundingBox expanded = cell.Box;
+                Vector3 border = new Vector3(10, 10, 10);
+                expanded.Min -= border;
+                expanded.Max += border;
+                foreach (Cell otherCell in cells)
+                {
+                    if (expanded.Intersects(otherCell.Box))
+                    {
+                        cell.Adjacent.Add(otherCell);
+                    }
+                }
+            }            
+        }
+
+        public int FindCell(Vector3 pos)
+        {
+            foreach (Cell cell in cells)
+            {
+                if (cell.Contains(pos))
+                {
+                    return cell.Number;
+                }
+            }
+            Console.WriteLine();
+            // Should not get here
+            return -1;
+        }
+
+        public void Partition()
+        {
+            List<Entity> entities = XNAGame.Instance().Children;
+            foreach (Entity entity in entities)
+            {
+                if (entity is Fighter)
+                {
+                    entity.Cell = FindCell(entity.pos);
+                }
+            }
+        }
+    }
+}
